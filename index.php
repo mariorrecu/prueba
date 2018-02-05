caf18cebc57b30cf1fe86badf9a0b3be18562511
Mario Requejo Cuevas <mariorrecu@hotmail.com>
2018-02-05 11:49:09 +0100

    segundo commit

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ and open the template in the editor.
     <body>
         <?php
             echo 'primera linea del proyecto';
+            
+            echo 'prueba segundo commit';
         ?>
     </body>
 </html>
